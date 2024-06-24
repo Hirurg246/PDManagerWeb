0749191c3a13824a49201bd7e192f1fb19c9c6eb
Yuriy Avdeev <avdieiev.iurii@gmail.com>
2024-06-24 14:48:18 +0500

    Removed unneeded FluentAPI

--- PDManagerWeb/Models/PDManagerContext.cs
+++ PDManagerWeb/Models/PDManagerContext.cs
@@ -50,18 +50,11 @@ public partial class PDManagerContext : DbContext
             entity.HasIndex(e => e.Login, "UK_Account_Login")
                 .IsUnique()
                 .HasFilter("([isDeleted]=(0))");
-
-            entity.Property(e => e.Id).UseIdentityColumn();
-            entity.Property(e => e.PasswordHash).IsFixedLength();
         });
 
         modelBuilder.Entity<AllowedFormat>(entity =>
         {
             entity.ToTable(tb => tb.HasTrigger("Trigger_AllowedFormat_Delete"));
-
-            //entity.HasOne(d => d.DocumentFormat).WithMany(p => p.AllowedFormats).HasConstraintName("FK_AllowedFormat_DocumentFormat");
-
-            //entity.HasOne(d => d.DocumentType).WithMany(p => p.AllowedFormats).HasConstraintName("FK_AllowedFormat_DocumentType");
         });
 
         modelBuilder.Entity<Product>(entity =>
@@ -73,41 +66,16 @@ public partial class PDManagerContext : DbContext
                 .HasFilter("([isDeleted]=(0))");
 
             entity.Property(e => e.StartDate).HasDefaultValueSql("(getdate())");
-
-            entity.HasOne(d => d.Head).WithMany(p => p.Products)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Product_Head");
-        });
-
-        modelBuilder.Entity<ProductAccess>(entity =>
-        {
-            entity.HasOne(d => d.AccessLevel).WithMany(p => p.ProductAccesses)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_ProductAccesse_AccessLevel");
-
-            //entity.HasOne(d => d.Account).WithMany(p => p.ProductAccesses).HasConstraintName("FK_ProductAccess_Account");
-
-            //entity.HasOne(d => d.Product).WithMany(p => p.ProductAccesses).HasConstraintName("FK_ProductAccess_Product");
         });
 
         modelBuilder.Entity<ProgramDocument>(entity =>
         {
             entity.Property(e => e.LastChangeDate).HasDefaultValueSql("(getdate())");
-
-            entity.HasOne(d => d.DocumentTypeFormat).WithMany(p => p.ProgramDocuments)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_ProgramDocument_TypeFormat");
-
-            //entity.HasOne(d => d.LastChangeUser).WithMany(p => p.ProgramDocuments).HasConstraintName("FK_ProgramDocument_ChangeUser");
-
-            //entity.HasOne(d => d.Product).WithMany(p => p.ProgramDocuments).HasConstraintName("FK_ProgramDocument_Product");
         });
 
         modelBuilder.Entity<SysAdmin>(entity =>
         {
             entity.Property(e => e.Id).ValueGeneratedNever();
-
-            entity.HasOne(d => d.IdNavigation).WithOne(p => p.SysAdmin).HasConstraintName("FK_SysAdmin_Account");
         });
 
         OnModelCreatingPartial(modelBuilder);
